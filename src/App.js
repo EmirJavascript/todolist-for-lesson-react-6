7d8d96b80003276f0691e77919a3c987d109464c
EmirJavascript <orozbekovjob@gmail.com>
2023-03-30 11:55:48 +0600

    add personal hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,65 @@
 import { useState } from 'react'
 import { TodoList } from './components/todo-list'
 import { startTodolist } from './data'
+import { useTodoList } from "./hooks/useTodoList";
+import styled from "styled-components"
+
+const StyledTodoList = styled(TodoList)`
+  background-color: beige;
+  margin-top: 50px;
+`
 
 function App() {
-  const [todos, setTodos] = useState(startTodolist)
+  // const [todos, setTodos] = useState(startTodolist)
+
+  const { todos, toggleTodo } = useTodoList(startTodolist)
 
   const getOverdueTodos = () => {
     const today = new Date()
-    return todos.filter((todo) => new Date(todo.deadline) < today)
+    return todos.filter((todo) => new Date(todo.deadline) < today && !todo.isDone)
   }
 
   const getActualTodos = () => {
     const today = new Date()
-    return todos.filter((todo) => new Date(todo.deadline) >= today)
+    return todos.filter((todo) => new Date(todo.deadline) >= today && !todo.isDone)
   }
 
-  const toggleTodo = (id) => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, isDone: !todo.isDone }
-      } else {
-        return todo
-      }
-    })
-    setTodos(updatedTodos)
+  const getCompletedTodos = () => {
+    return todos.filter((todo) => todo.isDone)
   }
+
+  // const toggleTodo = (id) => {
+  //   const updatedTodos = todos.map((todo) => {
+  //     if (todo.id === id) {
+  //       return { ...todo, isDone: !todo.isDone }
+  //     } else {
+  //       return todo
+  //     }
+  //   })
+  //   setTodos(updatedTodos)
+  // }
+
+  // const div = styled.div`
+  //   display: flex;
+
+  // `
 
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoList
+      <StyledTodoList
         title="Overdue"
         items={getOverdueTodos()}
         onToggleTodo={toggleTodo}
       />
-      <TodoList
+      <StyledTodoList
         title="Actual"
         items={getActualTodos()}
+        onToggleTodo={toggleTodo}
+      />
+      <StyledTodoList 
+        title="Completed"
+        items={getCompletedTodos()}
         onToggleTodo={toggleTodo}
       />
     </div>
--- src/components/todo-item/index.jsx
+++ src/components/todo-item/index.jsx
@@ -1,16 +1,45 @@
+import styled from "styled-components"
+
+const TodoContainer = styled.li`
+  background-color: #ccc;
+  list-style: none;
+  font-size: 18px;
+
+  &:hover {
+    color: red;
+  }
+
+  & > input {
+    margin-right: 10px;
+  }
+`
+
+// const InputContainer = styled.input`
+  
+// `
+
+const TodoText = styled.span`
+  text-decoration: ${(props) => {
+    if (props.isDone) return 'line-through' 
+    return 'none'
+  }};
+`
+
 export const TodoItem = ({ id, text, isDone, onToggleTodo }) => {
   const handleToggleTodo = () => {
     onToggleTodo?.(id)
   }
 
   return (
-    <li>
+    <TodoContainer>
       <input
         type="checkbox"
         checked={isDone}
         onChange={handleToggleTodo}
       />
-      <span style={{ textDecoration: isDone ? 'line-through' : 'none' }}>{text}</span>
-    </li>
+      <TodoText isDone={isDone}>
+          {text}
+      </TodoText>
+    </TodoContainer>
   )
 }
